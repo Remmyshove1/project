2027ee4ab8878e6db80fc2a3b8802fdeb690e47c
jessicapark <jpark32@umbc.edu>
2017-12-14 11:37:42 -0500

    Initial commit

---
+++ Person.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface Person
+//interface
+{
+    abstract String getFirstName();
+    abstract String getLastName();
+    abstract String getGender();
+    //abstraction #16
+
+}
